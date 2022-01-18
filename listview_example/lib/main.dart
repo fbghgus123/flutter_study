37897a9f82f7dc0b4b20e91473d847e271c227f4
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-01-18 20:37:36 +0900

    Study material design

--- listview_example/lib/main.dart
+++ listview_example/lib/main.dart
@@ -66,9 +66,14 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       body: TabBarView(
         children: <Widget>[
           FirstApp(list: animalList),
-          SecondApp()
+          SecondApp(list: animalList)
         ],
         controller: controller,
+      ),
+      bottomNavigationBar: TabBar(tabs: <Tab>[
+        Tab(icon: Icon(Icons.looks_one, color: Colors.blue),),
+        Tab(icon: Icon(Icons.looks_two, color:Colors.blue),)
+      ], controller: controller,
       )
     );
   }
